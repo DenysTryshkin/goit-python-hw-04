efd62a9c3ad876365e83e782bf47635bd38e877c
Denys Tryshkin <denystryshkin@gmail.com>
2026-01-16 20:59:38 -0500

    added task_1

---
+++ task_1/main.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+def total_salary(path):
+    total = 0
+    developers_count = 0
+    
+    try:
+        with open(path, mode="r", encoding="utf-8") as file:
+            for line in file:
+                _, salary = line.strip().split(",")
+                total += float(salary)
+                developers_count += 1
+
+        if developers_count == 0:
+            return 0, 0
+                
+        average_salary = total / developers_count
+        return total, average_salary
+
+    except (FileNotFoundError, ValueError) as e:
+        print(f"Error: {e}")
+        return 0, 0
+    
+total, average = total_salary("/Users/denys/goit-python-hw-04/task_1/salary_file.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+
