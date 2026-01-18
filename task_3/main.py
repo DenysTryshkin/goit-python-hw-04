0e6233f05aeecdf5219b7058070de7750f9112b3
Denys Tryshkin <denystryshkin@gmail.com>
2026-01-17 20:39:42 -0500

    added task_3

--- task_3/main.py
+++ task_3/main.py
@@ -1,3 +1,28 @@
 from colorama import Fore
 import sys
 from pathlib import Path
+
+def show_directory(directory_path, indent=0):
+    path = Path(directory_path)
+
+    if not path.exists() or not path.is_dir():
+          print(Fore.RED + f"The path '{directory_path}' does not exist or it is not a directory.")
+          return
+    
+    print(" " * indent + Fore.BLUE + f"📁 {path.name}/")
+
+    for item in path.iterdir():
+        if item.is_dir():
+              show_directory(item, indent = indent + 4)
+
+        elif item.is_file():
+             print(" " * (indent + 2) + Fore.GREEN + f"📄 {item.name}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(Fore.RED + "Please specify the path to the directory")
+        sys.exit(1)
+
+directory_path = sys.argv[1]
+show_directory(directory_path)
+
